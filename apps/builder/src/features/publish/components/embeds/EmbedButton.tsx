1f96bed67d8e38a5f17b132eccbc86b9b98ad524
Rodrigo Rodrigues <prorodrigorodrigues@gmail.com>
2024-05-08 15:04:48 -0300

    🔍 chore: Remove integration-related cards. These integrations have not been tested and were removed for validation before being made available.

--- apps/builder/src/features/publish/components/embeds/EmbedButton.tsx
+++ apps/builder/src/features/publish/components/embeds/EmbedButton.tsx
@@ -7,36 +7,36 @@ import {
   useColorModeValue,
 } from '@chakra-ui/react'
 import {
-  WordpressLogo,
-  ShopifyLogo,
-  WixLogo,
-  GtmLogo,
   JavascriptLogo,
-  ReactLogo,
   NotionLogo,
-  WebflowLogo,
   IframeLogo,
+  // WordpressLogo,
+  // ShopifyLogo,
+  // WixLogo,
+  // GtmLogo,
+  // ReactLogo,
+  // WebflowLogo,
 } from './logos'
 import React from 'react'
 import {
-  WordpressModal,
-  ShopifyModal,
-  WebflowModal,
-  GtmModal,
+  // WordpressModal,
+  // ShopifyModal,
+  // WebflowModal,
+  // GtmModal,
+  // ReactModal,
+  // WixModal,
   JavascriptModal,
-  ReactModal,
   NotionModal,
   IframeModal,
-  WixModal,
 } from './modals'
 import { ScriptModal } from './modals/Script/ScriptModal'
 import { CodeIcon } from '@/components/icons'
 import { ApiModal } from './modals/ApiModal'
 import { ScriptIcon } from '@/features/blocks/logic/script/components/ScriptIcon'
-import { FlutterFlowLogo } from './logos/FlutterFlowLogo'
-import { FlutterFlowModal } from './modals/FlutterFlowModal'
-import { NextjsLogo } from './logos/NextjsLogo'
-import { NextjsModal } from './modals/Nextjs/NextjsModal'
+// import { FlutterFlowLogo } from './logos/FlutterFlowLogo'
+// import { FlutterFlowModal } from './modals/FlutterFlowModal'
+// import { NextjsLogo } from './logos/NextjsLogo'
+// import { NextjsModal } from './modals/Nextjs/NextjsModal'
 import {
   WhatsAppLogo,
   whatsAppBrandColor,
@@ -47,8 +47,8 @@ import { useWorkspace } from '@/features/workspace/WorkspaceProvider'
 import { hasProPerks } from '@/features/billing/helpers/hasProPerks'
 import { LockTag } from '@/features/billing/components/LockTag'
 import { Plan } from '@typebot.io/prisma'
-import { FramerModal } from './modals/FramerModal'
-import { FramerLogo } from './logos/FramerLogo'
+// import { FramerModal } from './modals/FramerModal'
+// import { FramerLogo } from './logos/FramerLogo'
 
 export type ModalProps = {
   publicId: string
@@ -122,40 +122,40 @@ export const integrationsList = [
       </ParentModalProvider>
     )
   },
-  (props: Pick<ModalProps, 'publicId' | 'isPublished'>) => (
-    <EmbedButton
-      logo={<WordpressLogo height={100} width="70px" />}
-      label="Wordpress"
-      modal={({ onClose, isOpen }) => (
-        <WordpressModal isOpen={isOpen} onClose={onClose} {...props} />
-      )}
-      {...props}
-    />
-  ),
-  (props: Pick<ModalProps, 'publicId' | 'isPublished'>) => (
-    <EmbedButton
-      logo={<ShopifyLogo height={100} width="65px" />}
-      label="Shopify"
-      modal={(modalProps) => <ShopifyModal {...modalProps} {...props} />}
-      {...props}
-    />
-  ),
-  (props: Pick<ModalProps, 'publicId' | 'isPublished'>) => (
-    <EmbedButton
-      logo={<WixLogo height={100} width="90px" />}
-      label="Wix"
-      modal={(modalProps) => <WixModal {...modalProps} {...props} />}
-      {...props}
-    />
-  ),
-  (props: Pick<ModalProps, 'publicId' | 'isPublished'>) => (
-    <EmbedButton
-      logo={<GtmLogo height={100} width="70px" />}
-      label="Google Tag Manager"
-      modal={(modalProps) => <GtmModal {...modalProps} {...props} />}
-      {...props}
-    />
-  ),
+  // (props: Pick<ModalProps, 'publicId' | 'isPublished'>) => (
+  //   <EmbedButton
+  //     logo={<WordpressLogo height={100} width="70px" />}
+  //     label="Wordpress"
+  //     modal={({ onClose, isOpen }) => (
+  //       <WordpressModal isOpen={isOpen} onClose={onClose} {...props} />
+  //     )}
+  //     {...props}
+  //   />
+  // ),
+  // (props: Pick<ModalProps, 'publicId' | 'isPublished'>) => (
+  //   <EmbedButton
+  //     logo={<ShopifyLogo height={100} width="65px" />}
+  //     label="Shopify"
+  //     modal={(modalProps) => <ShopifyModal {...modalProps} {...props} />}
+  //     {...props}
+  //   />
+  // ),
+  // (props: Pick<ModalProps, 'publicId' | 'isPublished'>) => (
+  //   <EmbedButton
+  //     logo={<WixLogo height={100} width="90px" />}
+  //     label="Wix"
+  //     modal={(modalProps) => <WixModal {...modalProps} {...props} />}
+  //     {...props}
+  //   />
+  // ),
+  // (props: Pick<ModalProps, 'publicId' | 'isPublished'>) => (
+  //   <EmbedButton
+  //     logo={<GtmLogo height={100} width="70px" />}
+  //     label="Google Tag Manager"
+  //     modal={(modalProps) => <GtmModal {...modalProps} {...props} />}
+  //     {...props}
+  //   />
+  // ),
   (props: Pick<ModalProps, 'publicId' | 'isPublished'>) => (
     <EmbedButton
       logo={<JavascriptLogo height={100} width="70px" />}
@@ -164,22 +164,22 @@ export const integrationsList = [
       {...props}
     />
   ),
-  (props: Pick<ModalProps, 'publicId' | 'isPublished'>) => (
-    <EmbedButton
-      logo={<ReactLogo height={100} width="70px" />}
-      label="React"
-      modal={(modalProps) => <ReactModal {...modalProps} {...props} />}
-      {...props}
-    />
-  ),
-  (props: Pick<ModalProps, 'publicId' | 'isPublished'>) => (
-    <EmbedButton
-      logo={<NextjsLogo height={100} width="70px" />}
-      label="Nextjs"
-      modal={(modalProps) => <NextjsModal {...modalProps} {...props} />}
-      {...props}
-    />
-  ),
+  // (props: Pick<ModalProps, 'publicId' | 'isPublished'>) => (
+  //   <EmbedButton
+  //     logo={<ReactLogo height={100} width="70px" />}
+  //     label="React"
+  //     modal={(modalProps) => <ReactModal {...modalProps} {...props} />}
+  //     {...props}
+  //   />
+  // ),
+  // (props: Pick<ModalProps, 'publicId' | 'isPublished'>) => (
+  //   <EmbedButton
+  //     logo={<NextjsLogo height={100} width="70px" />}
+  //     label="Nextjs"
+  //     modal={(modalProps) => <NextjsModal {...modalProps} {...props} />}
+  //     {...props}
+  //   />
+  // ),
   (props: Pick<ModalProps, 'publicId' | 'isPublished'>) => (
     <EmbedButton
       logo={<CodeIcon height={100} width="60px" />}
@@ -188,38 +188,30 @@ export const integrationsList = [
       {...props}
     />
   ),
-  (props: Pick<ModalProps, 'publicId' | 'isPublished'>) => (
-    <EmbedButton
-      logo={<NotionLogo height={100} width="60px" />}
-      label="Notion"
-      modal={(modalProps) => <NotionModal {...modalProps} {...props} />}
-      {...props}
-    />
-  ),
-  (props: Pick<ModalProps, 'publicId' | 'isPublished'>) => (
-    <EmbedButton
-      logo={<WebflowLogo height={100} width="70px" />}
-      label="Webflow"
-      modal={(modalProps) => <WebflowModal {...modalProps} {...props} />}
-      {...props}
-    />
-  ),
-  (props: Pick<ModalProps, 'publicId' | 'isPublished'>) => (
-    <EmbedButton
-      logo={<FlutterFlowLogo height={100} width="60px" />}
-      label="FlutterFlow"
-      modal={(modalProps) => <FlutterFlowModal {...modalProps} {...props} />}
-      {...props}
-    />
-  ),
-  (props: Pick<ModalProps, 'publicId' | 'isPublished'>) => (
-    <EmbedButton
-      logo={<FramerLogo height={100} width="60px" />}
-      label="Framer"
-      modal={(modalProps) => <FramerModal {...modalProps} {...props} />}
-      {...props}
-    />
-  ),
+  // (props: Pick<ModalProps, 'publicId' | 'isPublished'>) => (
+  //   <EmbedButton
+  //     logo={<WebflowLogo height={100} width="70px" />}
+  //     label="Webflow"
+  //     modal={(modalProps) => <WebflowModal {...modalProps} {...props} />}
+  //     {...props}
+  //   />
+  // ),
+  // (props: Pick<ModalProps, 'publicId' | 'isPublished'>) => (
+  //   <EmbedButton
+  //     logo={<FlutterFlowLogo height={100} width="60px" />}
+  //     label="FlutterFlow"
+  //     modal={(modalProps) => <FlutterFlowModal {...modalProps} {...props} />}
+  //     {...props}
+  //   />
+  // ),
+  // (props: Pick<ModalProps, 'publicId' | 'isPublished'>) => (
+  //   <EmbedButton
+  //     logo={<FramerLogo height={100} width="60px" />}
+  //     label="Framer"
+  //     modal={(modalProps) => <FramerModal {...modalProps} {...props} />}
+  //     {...props}
+  //   />
+  // ),
   (props: Pick<ModalProps, 'publicId' | 'isPublished'>) => (
     <EmbedButton
       logo={
@@ -239,6 +231,14 @@ export const integrationsList = [
       logo={<IframeLogo height={100} width="70px" />}
       label="Iframe"
       modal={(modalProps) => <IframeModal {...modalProps} {...props} />}
+      {...props}
+    />
+  ),
+  (props: Pick<ModalProps, 'publicId' | 'isPublished'>) => (
+    <EmbedButton
+      logo={<NotionLogo height={100} width="60px" />}
+      label="Notion"
+      modal={(modalProps) => <NotionModal {...modalProps} {...props} />}
       {...props}
     />
   ),
