407e5ccb86b175ed657191aaa6ae8176e9c31a26
Rodrigo Rodrigues <prorodrigorodrigues@gmail.com>
2024-06-30 14:27:44 -0300

    🔧 chore: Remove logs for the flow termination feature

--- packages/bot-engine/whatsapp/resumeWhatsAppFlow.ts
+++ packages/bot-engine/whatsapp/resumeWhatsAppFlow.ts
@@ -88,24 +88,6 @@ export const resumeWhatsAppFlow = async ({
     })
   }
 
-  var dataRequest5 = {
-    tipo: 'reply@startWhatsAppSession',
-    reply,
-  }
-  await fetch(
-    `https://wfv2-dev07.workfacilit.com/app/prod/api/demandas/inserir-log`,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Atend-Token': 'WF',
-        Authorization:
-          'Basic ODM1VFJHREhTNjNVSEY4NDdISERKM1U3OjI3NjRIRkpTS1M4NTZSSk1KRDg3M1lFTUQ3',
-      },
-      body: JSON.stringify(dataRequest5),
-    }
-  )
-
   let resumeResponse
 
   if (reply === '/sair' && workspaceId) {
