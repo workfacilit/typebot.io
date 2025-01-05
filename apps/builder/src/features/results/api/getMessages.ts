5381ae0dbbad33ccbc7ca7561fb16a2dbb16fc77
Rodrigo Rodrigues <prorodrigorodrigues@gmail.com>
2025-01-05 19:20:41 -0300

    ✨ feat: Adiciona endpoint para listar mensagens de resultado
    Implementa a rota GET para obter mensagens de um resultado específico.
    Define o schema de mensagem e integra a nova funcionalidade ao router de resultados.

---
+++ apps/builder/src/features/results/api/getMessages.ts
@@ -0,0 +1,34 @@
+import prisma from '@typebot.io/lib/prisma'
+import { authenticatedProcedure } from '@/helpers/server/trpc'
+import { messageSchema } from '@typebot.io/schemas'
+import { z } from 'zod'
+
+export const getMessages = authenticatedProcedure
+  .meta({
+    openapi: {
+      method: 'GET',
+      path: '/v1/typebots/results/{resultId}/messages',
+      protect: true,
+      summary: 'List result messages',
+      tags: ['Results'],
+    },
+  })
+  .input(
+    z.object({
+      resultId: z.string(),
+    })
+  )
+  .output(z.object({ messages: z.array(messageSchema) }))
+  .query(async ({ input: { resultId } }) => {
+    const messages = await prisma.messageLog.findMany({
+      where: {
+        resultId: resultId,
+      },
+      orderBy: {
+        timestamp: 'asc',
+      },
+    })
+    if (!messages) throw new Error('messages not found')
+
+    return { messages: messages }
+  })
