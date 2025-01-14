ff461a7e85d301dd6909baafdf0aa2f95bcc5a50
Rodrigo Rodrigues <prorodrigorodrigues@gmail.com>
2025-01-13 21:35:45 -0300

    🐛 fix: Adiciona tratamento de erros ao enviar mensagens pelo WhatsApp
    Envolve a requisição `ky.post` em um bloco try-catch para capturar erros durante o envio de mensagens e envia logs detalhados para facilitar o diagnóstico.

--- packages/bot-engine/whatsapp/sendWhatsAppMessage.ts
+++ packages/bot-engine/whatsapp/sendWhatsAppMessage.ts
@@ -24,19 +24,26 @@ export const sendWhatsAppMessage = async ({
   resultIdWA,
   typebotId,
 }: Props) => {
-  ky.post(
-    `${env.WHATSAPP_CLOUD_API_URL}/v17.0/${credentials.phoneNumberId}/messages`,
-    {
-      headers: {
-        Authorization: `Bearer ${credentials.systemUserAccessToken}`,
-      },
-      json: {
-        messaging_product: 'whatsapp',
-        to,
-        ...message,
-      },
-    }
-  )
+  try {
+    await ky.post(
+      `${env.WHATSAPP_CLOUD_API_URL}/v17.0/${credentials.phoneNumberId}/messages`,
+      {
+        headers: {
+          Authorization: `Bearer ${credentials.systemUserAccessToken}`,
+        },
+        json: {
+          messaging_product: 'whatsapp',
+          to,
+          ...message,
+        },
+      }
+    )
+  } catch (error) {
+    await sendLogRequest(
+      'errorSendingWhatsAppMessage@sendWhatsAppMessage',
+      error
+    )
+  }
   if (workspaceId) {
     try {
       await logMessage(
